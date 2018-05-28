246f7388a1d8c4594d8ee18557134e74ebfd078f
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-05-28 18:57:03 +0200

    Fix linter.

--- test/engineHelpers.test.js
+++ test/engineHelpers.test.js
@@ -9,9 +9,9 @@ test('engineHelpers: isExpression()', t => {
 
 test('engineHelpers: valueFromText', t => {
   // TODO: add more of thi
-  t.deepEqual(valueFromText('false'), { type: "boolean", data: false }, 'valueFromText should provide a correct unpacked value')
-  t.deepEqual(valueFromText('true'), { type: "boolean", data: true }, 'valueFromText should provide a correct unpacked value')
-  t.deepEqual(valueFromText('1'), { type: "integer", data: 1 }, 'valueFromText should provide a correct unpacked value')
-  t.deepEqual(valueFromText('1.2'), { type: "number", data: 1.2 }, 'valueFromText should provide a correct unpacked value')
+  t.deepEqual(valueFromText('false'), { type: 'boolean', data: false }, 'valueFromText should provide a correct unpacked value')
+  t.deepEqual(valueFromText('true'), { type: 'boolean', data: true }, 'valueFromText should provide a correct unpacked value')
+  t.deepEqual(valueFromText('1'), { type: 'integer', data: 1 }, 'valueFromText should provide a correct unpacked value')
+  t.deepEqual(valueFromText('1.2'), { type: 'number', data: 1.2 }, 'valueFromText should provide a correct unpacked value')
   t.end()
 })
